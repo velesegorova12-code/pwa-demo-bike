21e47604531c38afdb04789536cbb10ad93161b9
velesegorova12-code <veles.egorova12@gmail.com>
2026-04-24 15:46:10 +0300

    chore: fix import order to satisfy linter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
+import { Toaster } from 'react-hot-toast'
+
 import { AppHeaderActions } from '@app/navigation/AppHeaderActions'
 import { AppLayout } from '@components/Layout'
 import { Outlet } from '@lib/router'
 
 function App() {
   return (
-    <AppLayout headerActions={<AppHeaderActions />}>
-      <Outlet />
-    </AppLayout>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <AppLayout headerActions={<AppHeaderActions />}>
+        <Outlet />
+      </AppLayout>
+    </>
   )
 }
 
