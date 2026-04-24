08ba9c8ea6187b8a7c4eaa43d1f6713d4fe1ad15
velesegorova12-code <veles.egorova12@gmail.com>
2026-04-24 22:40:43 +0300

    fix: i18next config and translation keys leakage

--- i18next-cli.config.ts
+++ i18next-cli.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'i18next-cli'
 
 export default defineConfig({
-  locales: ['en', 'et'],
+  locales: ['en'],
   extract: {
     input: ['src/**/*.{ts,tsx}'],
     output: 'public/locales/{{language}}/{{namespace}}.json',
